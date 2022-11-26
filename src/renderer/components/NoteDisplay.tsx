104b42e9923af95d570c20e1f4ba28895626242e
Chris Rimes <chris.rimes@gmail.com>
2022-11-26 21:50:07 +0000

    Add code syntax highlighting

--- src/renderer/components/NoteDisplay.tsx
+++ src/renderer/components/NoteDisplay.tsx
@@ -1,15 +1,28 @@
 /* eslint no-console: off */
 import DOMPurify from 'dompurify';
+import hljs from 'highlight.js';
 import { marked } from 'marked';
 import React from 'react';
 import { NoteKey } from '../model';
+import './NoteDisplay.css';
 
 interface Props {
   note: NoteKey | undefined;
 }
 
+marked.setOptions({
+  highlight: (code, lang) => {
+    const result = hljs.highlight(lang, code);
+    if (result.errorRaised) {
+      throw result.errorRaised;
+    }
+    return result.value;
+  },
+  langPrefix: 'hljs language-',
+});
+
 /**
- * Displays raw test of a note.
+ * Displays a formatted note.
  */
 const NoteDisplay = ({ note }: Props) => {
   const [content, setContent] = React.useState<string | undefined>(undefined);
