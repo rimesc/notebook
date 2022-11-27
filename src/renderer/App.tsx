cb87e042621ee9f95a1c70744d32c5d284ef0359
Chris Rimes <chris.rimes@gmail.com>
2022-11-27 16:00:44 +0000

    Add a button to toggle between edit and view modes
    Currently does nothing except change appearance based on the current mode.

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,4 +1,5 @@
-import { Box } from '@mui/material';
+import { Edit, EditOff } from '@mui/icons-material';
+import { Box, IconButton, Toolbar } from '@mui/material';
 import React from 'react';
 import { MemoryRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
@@ -10,12 +11,29 @@ const drawerWidth = 240;
 
 const Hello = () => {
   const [selected, setSelected] = React.useState<NoteKey | undefined>(undefined);
+  const [mode, setMode] = React.useState<'edit' | 'view'>('view');
+
+  const toggleMode = () => setMode(mode === 'view' ? 'edit' : 'view');
 
   return (
     <Box sx={{ display: 'flex' }}>
       <Sidebar width={drawerWidth} selected={selected} onSelect={setSelected} />
-      <Box component="main" sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}>
-        <NoteDisplay note={selected} />
+      <Box sx={{ display: 'flex', flexDirection: 'column', width: { sm: `calc(100% - ${drawerWidth}px)` } }}>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'end' }}>
+          <IconButton
+            edge="end"
+            color="inherit"
+            aria-label={mode === 'view' ? 'Edit' : 'View'}
+            sx={{ ml: 2 }}
+            disabled={!selected}
+            onClick={toggleMode}
+          >
+            {mode === 'view' ? <Edit /> : <EditOff />}
+          </IconButton>
+        </Toolbar>
+        <Box component="main" sx={{ flexGrow: 1, p: 3, pt: 0 }}>
+          <NoteDisplay note={selected} />
+        </Box>
       </Box>
     </Box>
   );
