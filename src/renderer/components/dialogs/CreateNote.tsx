d73bf19aaef0d6baf3bc0760cf0813ec8962fe25
Chris Rimes <chris.rimes@gmail.com>
2023-03-14 19:43:31 +0000

    Validate the name against the list of existing notes
    It obviously might still fail when we actually try to create the file, but this should catch most cases and it's easier to handle (and probably more responsive) than trying to asynchronously validate over IPC.

--- src/renderer/components/dialogs/CreateNote.tsx
+++ src/renderer/components/dialogs/CreateNote.tsx
@@ -3,19 +3,32 @@ import { useEffect, useState } from 'react';
 
 const CreateNote = () => {
   const [folder, setFolder] = useState<string | undefined>(undefined);
+  const [files, setFiles] = useState<string[] | undefined>(undefined);
   const [name, setName] = useState('');
 
-  useEffect(
-    () =>
-      window.electron.ipcRenderer.on('dialogs:create-note:init', (inFolder) => {
-        setFolder(inFolder as string);
-      }),
-    []
-  );
+  useEffect(() => {
+    return window.electron.ipcRenderer.on('dialogs:create-note:init', (inFolder) => {
+      setFolder(inFolder as string);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (folder) {
+      window.electron
+        .listNotes(folder)
+        .then((fs) => setFiles(fs.map((f) => f.toLowerCase())))
+        .catch(console.log);
+    }
+  }, [folder]);
+
+  // A name can be invalid but not conflicting if (a) it is empty or (b) the list of existing files hasn't
+  // been fetched yet. In these cases we don't display an error, but we also don't allow the name to be confirmed.
+  const isValid = name && files ? !files.includes(name.trim().toLowerCase()) : false;
+  const isConflicting = name && files ? !isValid : false;
 
   const handleEnter = () => {
-    if (folder && name) {
-      window.electron.ipcRenderer.sendMessage('dialogs:create-note:done', folder, name);
+    if (isValid) {
+      window.electron.ipcRenderer.sendMessage('dialogs:create-note:done', folder, name.trim());
     }
   };
   const handleEscape = () => window.electron.ipcRenderer.sendMessage('dialogs:create-note:done');
@@ -47,9 +60,10 @@ const CreateNote = () => {
         variant="standard"
         value={name}
         placeholder="New note"
-        helperText="Press 'Enter' to confirm or 'Escape' to cancel"
         fullWidth
         autoFocus
+        error={isConflicting}
+        helperText={isConflicting ? 'Please choose a unique name' : "Press 'Enter' to confirm or 'Escape' to cancel"}
         onChange={(e) => setName(e.currentTarget.value)}
         onKeyUp={(e) => handleKeyPress(e.key)}
       />
