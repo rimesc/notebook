a3c55690d49f40bc919b65fe282d1f6fa8f1581a
Chris Rimes <chris.rimes@gmail.com>
2023-03-18 16:52:53 +0000

    Simplify async code

--- src/main/files.ts
+++ src/main/files.ts
@@ -21,38 +21,20 @@ function isMarkdown(p: string): boolean {
 
 async function listFiles(dir: string): Promise<string[]> {
   return new Promise<string[]>((resolve, reject) => {
-    fs.readdir(dir, (err, files) => {
-      if (err) {
-        reject(err);
-      } else {
-        // return the full path to the file
-        resolve(files.map((name) => path.join(dir, name)));
-      }
-    });
+    // return the full path to each file
+    fs.readdir(dir, (err, files) => (err ? reject(err) : resolve(files.map((name) => path.join(dir, name)))));
   });
 }
 
 async function readFile(file: string, encoding: BufferEncoding): Promise<string> {
   return new Promise<string>((resolve, reject) => {
-    fs.readFile(file, (err, buffer) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(buffer.toString(encoding));
-      }
-    });
+    fs.readFile(file, (err, buffer) => (err ? reject(err) : resolve(buffer.toString(encoding))));
   });
 }
 
 async function writeFile(file: string, content: string, encoding: BufferEncoding): Promise<void> {
   return new Promise<void>((resolve, reject) => {
-    fs.writeFile(file, Buffer.from(content, encoding), (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
+    fs.writeFile(file, Buffer.from(content, encoding), (err) => (err ? reject(err) : resolve()));
   });
 }
 
@@ -68,17 +50,11 @@ async function moveFile(from: string, to: string): Promise<void> {
     throw new Error(`File '${to}' already exists`);
   }
   const parent = path.dirname(to);
-  if (!fs.existsSync(parent) || !fs.lstatSync(parent).isDirectory()) {
+  if (!fs.existsSync(parent) || !isDirectory(parent)) {
     throw new Error(`Directory '${parent}' does not exist or is not a directory`);
   }
   return new Promise<void>((resolve, reject) => {
-    fs.rename(from, to, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
+    fs.rename(from, to, (err) => (err ? reject(err) : resolve()));
   });
 }
 
