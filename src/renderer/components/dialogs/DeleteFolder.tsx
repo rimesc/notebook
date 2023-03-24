42988d9c9edd4635a4aa9d6a551274fa12b36764
Chris Rimes <chris.rimes@gmail.com>
2023-03-24 15:55:18 +0000

    Extract common component for confirmation dialogs

--- src/renderer/components/dialogs/DeleteFolder.tsx
+++ src/renderer/components/dialogs/DeleteFolder.tsx
@@ -1,6 +1,6 @@
-import { Box, Button, Typography } from '@mui/material';
 import ellipsize from 'ellipsize';
 import { useEffect, useState } from 'react';
+import ConfirmationDialog from './ConfirmationDialog';
 
 export default function DeleteFolder() {
   const [folder, setFolder] = useState<string | undefined>(undefined);
@@ -11,52 +11,12 @@ export default function DeleteFolder() {
     });
   }, []);
 
-  const handleSubmit = () => {
-    if (folder) {
-      window.electron.deleteFolder(folder);
-      window.electron.closeDialog();
-    }
-  };
-
-  return (
-    <Box
-      sx={{
-        p: 2,
-        height: '100vh',
-        width: 1,
-        boxSizing: 'border-box',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-      }}
-    >
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          flexGrow: 1,
-          alignItems: 'center',
-        }}
-      >
-        <Typography component="h2" variant="h6" sx={{ textAlign: 'center', overflow: 'hidden' }}>
-          Delete folder &apos;{ellipsize(folder, 50)}&apos;?
-        </Typography>
-      </Box>
-      <Box
-        sx={{
-          width: 1,
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-around',
-        }}
-      >
-        <Button onClick={window.electron.closeDialog}>Cancel</Button>
-        <Button color="error" onClick={handleSubmit}>
-          Delete
-        </Button>
-      </Box>
-    </Box>
-  );
+  return folder ? (
+    <ConfirmationDialog
+      action="Delete"
+      danger
+      title={`Delete folder '${ellipsize(folder, 50)}'?`}
+      onConfirm={() => window.electron.deleteFolder(folder)}
+    />
+  ) : null;
 }
